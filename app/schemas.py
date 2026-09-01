7e90723673243766ed7eba9c425e4c12688995b0
drestrepo242 <TU_CORREO_DE_DRESTREPO242@email.com>
2026-09-01 10:51:25 -0500

    fix: import UUID, datetime and ConfigDict in schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,7 @@
-from pydantic import BaseModel
+from uuid import UUID
+from datetime import datetime
 from typing import Optional
+from pydantic import BaseModel, ConfigDict
 
 class ExtractedProductData(BaseModel):
     search_keyword: str
